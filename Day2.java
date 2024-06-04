26b746083001ba4bc7c30185d7f0f34a8e235acb
Aryan Kumar <106177870+aaravkumar02@users.noreply.github.com>
2024-06-04 20:56:21 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,30 @@
+public class Day2 {
+
+    class car {
+
+        String brand;
+        String car_color;
+
+        void drive() {
+            System.out.println("This car is driving");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("aryan");
+        // car mycar1= new car();
+
+        // implicit
+        long big = 45;
+        float ddec = 3;
+        double d = 3.4f;
+
+        // explicit
+        float dec1 = (float) 3.4;
+        long dec = (long) 3.4;
+        int eint = (int) 3.4;
+
+        System.out.println(eint);
+
+    }
+}
